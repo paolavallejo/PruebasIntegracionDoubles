6a86f1fbf51fee78513614e704ad5d3dac26f38a
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-04-05 14:26:38 +0000

    Paso 0: Preparación del entorno

--- database.py
+++ database.py
@@ -2,9 +2,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///orders.db"
-
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine("sqlite:///orders.db")
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
