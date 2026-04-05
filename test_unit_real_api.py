726d939b2a0dba16839a133550f61788504c2528
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-04-05 14:55:52 +0000

    PASO 6: Tests “normales” con API REAL (con solución)

--- test_unit_real_api.py
+++ test_unit_real_api.py
@@ -7,12 +7,12 @@ Base.metadata.create_all(bind=engine)
 class DummyLogger:
     def log(self, msg):
         # TODO: implementar un logger que no haga nada
-        __________
+        pass
 
 class NullNotifier:
     def send(self, to, message):
         # TODO: implementar un notifier que no haga nada
-        __________
+        pass
 
 def test_create_order_with_real_api():
     db = SessionLocal()
@@ -27,6 +27,6 @@ def test_create_order_with_real_api():
     )
 
     # TODO: completar el estado esperado
-    assert order.status == "________"
+    assert order.status == "CREATED"
 
     db.close()
