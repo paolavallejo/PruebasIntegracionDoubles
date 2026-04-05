b080ec8f4217f73af4f42dd12a385499575a1b08
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-04-05 15:26:37 +0000

    Paso 7: Romper la dependencia externa

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request
 from database import Base, engine, SessionLocal
 from order_service import create_order
 from models import Order
-#from user_repository import JsonPlaceholderUserRepository
-from user_repository import FakeUserRepository
+from user_repository import JsonPlaceholderUserRepository
+#from user_repository import FakeUserRepository
 
 app = Flask(__name__)
 Base.metadata.create_all(bind=engine)
@@ -20,8 +20,8 @@ class WebLogger:
 @app.route("/", methods=["GET", "POST"])
 def index():
     db = SessionLocal()
-    #repo = JsonPlaceholderUserRepository()
-    repo = FakeUserRepository()
+    repo = JsonPlaceholderUserRepository()
+    #repo = FakeUserRepository()
     result = None
     error = None
 
