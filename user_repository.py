ac629dd72bbdd14e6f1f654be86b4542954af511
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-04-05 14:30:34 +0000

    Paso 3: Repositorio REAL de usuarios

--- user_repository.py
+++ user_repository.py
@@ -1,23 +1,11 @@
 # user_repository.py
 import requests
 
-"""
 class JsonPlaceholderUserRepository:
-    def get_user_email(self, user_id: int) -> str:
+    def get_user_email(self, user_id):
         response = requests.get(
             f"https://jsonplaceholder.typicode.com/users/{user_id}",
             timeout=5
         )
         response.raise_for_status()
         return response.json()["email"]
-"""
-
-"""
-class JsonPlaceholderUserRepository:
-    def get_user_email(self, user_id: int) -> str:
-        raise ConnectionError("User service unavailable")
-"""
-
-class FakeUserRepository:
-    def get_user_email(self, user_id: int) -> str:
-        return f"user{user_id}@fake.local"
