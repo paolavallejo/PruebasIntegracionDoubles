ea0195da67bf5af382a1618283b3afe55914ebac
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-04-05 14:47:55 +0000

    Paso 6: Tests “normales” con API REAL (con TODO)

--- test_unit_real_api.py
+++ test_unit_real_api.py
@@ -1,14 +1,30 @@
+from order_service import create_order
+from database import SessionLocal, Base, engine
+from user_repository import JsonPlaceholderUserRepository
+
+Base.metadata.create_all(bind=engine)
+
+class DummyLogger:
+    def log(self, msg):
+        pass
+
+class NullNotifier:
+    def send(self, to, message):
+        pass
+
 def test_create_order_with_real_api():
     db = SessionLocal()
 
     order = create_order(
         user_id=1,
         amount=100,
-        notifier=SpyNotifier(),
+        notifier=NullNotifier(),
         logger=DummyLogger(),
         db=db,
         user_repository=JsonPlaceholderUserRepository()
     )
 
-    assert order.status == "CREATED"
+    # TODO: completar el estado esperado
+    assert order.status == "________"
+
     db.close()
